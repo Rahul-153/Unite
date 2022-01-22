c4426ec5962a1f933a659632090e983e5f151c3f
unknown <muskan626bidani@gmail.com>
2022-01-23 00:33:46 +0530

    images changed

--- src/components/Main.js
+++ src/components/Main.js
@@ -16,133 +16,24 @@ function Main() {
         <p>This site will let you create, grow and enhance your skills</p>
       </div>
       <div className="main-cards">
+        <h1 className="main-heading">What We Provide ?</h1>
         <div className="posts">
           <div className="post1">
-          <Card/>
-          <Card/></div>
+          <Card src="https://makeskill.in/wp-content/uploads/2020/01/workshops.jpg" title="Workshops" text="  Donec eget ex magna. Interdum et malesuada fames ac ante ipsum
+                primis in faucibus. Pellentesque venenatis dolor imperdiet dolor
+                mattis sagittis magna etiam." 
+            
+                />
+          <Card src="https://s3-ap-south-1.amazonaws.com/static.awfis.com/wp-content/uploads/2017/07/07184649/ProjectManagement.jpg" title="Projects" text="  Donec eget ex magna. Interdum et malesuada fames ac ante ipsum
+                primis in faucibus. Pellentesque venenatis dolor imperdiet dolor
+                mattis sagittis magna etiam."/></div>
           <div className="post1">
-          <Card/>
-          <Card/></div>
-          {/* <div className="card-container">
-            <div className="card1">
-              <header>
-                <h1>Projects</h1>
-              </header>
-              <div>
-                <img src="https://bit.ly/3KqXSN0" className="post-img" />
-              </div>
-              <p>
-                Donec eget ex magna. Interdum et malesuada fames ac ante ipsum
+          <Card src="https://static.javatpoint.com/tutorial/group-discussion/images/group-discussion-tips.jpg" title="Discussions" text="  Donec eget ex magna. Interdum et malesuada fames ac ante ipsum
                 primis in faucibus. Pellentesque venenatis dolor imperdiet dolor
-                mattis sagittis magna etiam.
-              </p>
-              <div className="know-container">
-                {" "}
-                <Link to="/" className="know">
-                  Know More
-                </Link>
-              </div>
-            </div>
-            <div className="card1">
-              <header>
-                <h1>Workshops</h1>
-              </header>
-              <div>
-                <img src="https://bit.ly/3KqXSN0" className="post-img" />
-              </div>
-              <p>
-                Donec eget ex magna. Interdum et malesuada fames ac ante ipsum
+                mattis sagittis magna etiam."/>
+          <Card src="https://opencollective-production.s3-us-west-1.amazonaws.com/216d2c60-67e7-11e9-b984-89e2dc4d10f9.jpg" title="Community" text="  Donec eget ex magna. Interdum et malesuada fames ac ante ipsum
                 primis in faucibus. Pellentesque venenatis dolor imperdiet dolor
-                mattis sagittis magna etiam.
-              </p>
-              <div className="know-container">
-                {" "}
-                <Link to="/" className="know">
-                  Know More
-                </Link>
-              </div>
-            </div>
-          </div>
-          <div className="card-container">
-            <div className="card1">
-              <header>
-                <h1>Discussions Forum</h1>
-              </header>
-              <div>
-                <img src="https://bit.ly/3KqXSN0" className="post-img" />
-              </div>
-              <p>
-                Donec eget ex magna. Interdum et malesuada fames ac ante ipsum
-                primis in faucibus. Pellentesque venenatis dolor imperdiet dolor
-                mattis sagittis magna etiam.
-              </p>
-              <div className="know-container">
-                {" "}
-                <Link to="/" className="know">
-                  Know More
-                </Link>
-              </div>
-            </div>
-            <div className="card1">
-              <header>
-                <h1>Community</h1>
-              </header>
-              <div>
-                <img src="https://bit.ly/3KqXSN0" className="post-img" />
-              </div>
-              <p>
-                Donec eget ex magna. Interdum et malesuada fames ac ante ipsum
-                primis in faucibus. Pellentesque venenatis dolor imperdiet dolor
-                mattis sagittis magna etiam.
-              </p>
-              <div className="know-container">
-                {" "}
-                <Link to="/" className="know">
-                  Know More
-                </Link>
-              </div>
-            </div>
-          </div>
-          <div className="card-container">
-            <div className="card1">
-              <header>
-                <h1>Wanna Learn</h1>
-              </header>
-              <div>
-                <img src="https://bit.ly/3KqXSN0" className="post-img" />
-              </div>
-              <p>
-                Donec eget ex magna. Interdum et malesuada fames ac ante ipsum
-                primis in faucibus. Pellentesque venenatis dolor imperdiet dolor
-                mattis sagittis magna etiam.
-              </p>
-              <div className="know-container">
-                {" "}
-                <Link to="/" className="know">
-                  Know More
-                </Link>
-              </div>
-            </div>
-            <div className="card1">
-              <header>
-                <h1>Workshops</h1>
-              </header>
-              <div>
-                <img src="https://bit.ly/3KqXSN0" className="post-img" />
-              </div>
-              <p>
-                Donec eget ex magna. Interdum et malesuada fames ac ante ipsum
-                primis in faucibus. Pellentesque venenatis dolor imperdiet dolor
-                mattis sagittis magna etiam.
-              </p>
-              <div className="know-container">
-                {" "}
-                <Link to="/" className="know">
-                  Know More
-                </Link>
-              </div>
-            </div>
-          </div> */}
+                mattis sagittis magna etiam."/></div>
         </div>
       </div>
     </div>
